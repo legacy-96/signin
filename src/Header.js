b3142110683964bf2786841157bc00a0ded5dd7f
Thandile Nododile <6636345e@gmail.com>
2021-09-08 23:05:27 +0200

    removed sign in option: if a user has already signed in then he or she can logout not sign in again

--- src/Header.js
+++ src/Header.js
@@ -19,14 +19,13 @@ function Header({handleLogout}) {
         
           <div className="header__option">
             <span className="header__optionLineOne">Hello Guest</span>
-            <span className="header__optionLineTwo">Sign In</span>
           </div>
         
 
         
           <div className="header__option">
             <span className="header__optionLineOne">Returns</span>
-            <span className="header__optionLineTwo">& Orders</span>
+            <span className="header__optionLineTwo">Orders</span>
           </div>
         
           <div className="header__optionBasket">
